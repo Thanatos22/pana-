c078f7f8c017920a85e27210c7a5c355befaf783
Thanatos22 <76633120+Thanatos22@users.noreply.github.com>
2021-01-29 23:24:09 -0300

    Add files via upload

---
+++ src/dono.js
@@ -0,0 +1,9 @@
+const dono = (prefix) => {
+	return ` este é meu papai 🥰
+	mais conhecido como tamatos uor
+    até onde eu saiba ele me fez com muito amor e carinho 
+    não zoem o Nick dele ok!? ele fica tristinho
+
+}
+
+exports.dono = dono
--- src/help.js
+++ src/help.js
@@ -1,83 +1,33 @@
 const help = (prefix) => {
-	return `> *Sticker Commands* <
-command : *${prefix}sticker* or *${prefix}stiker*
-desc : convert image/gif/video to sticker
-usage : reply image/gif/video, or send image/gif/video with caption\n
-command : *${prefix}sticker nobg* or *${prefix}stiker nobg*
-desc : convert image to sticker while removing the background
-usage : reply image, or send image with caption\n
-command : *${prefix}toimg*
-desc : convert sticker to image
-usage : reply sticker\n
-command : *${prefix}tsticker* or *${prefix}tstiker*
-desc : convert text to sticker
-usage : *${prefix}tsticker text in here*\n
-> *Meme Commands* <
-command : *${prefix}meme*
-desc : random meme images [english]
-usage : just send the command\n
-command : *${prefix}memeindo*
-desc : random meme images [indo]
-usage : just send the command\n
-> *Others Commands* <
-command : *${prefix}gtts*
-desc : convert text to speech/audio
-usage : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts ja On2-chan*\n
-command : *${prefix}loli*
-desc : random loli images
-usage : just send the command\n
-command : *${prefix}nsfwloli*
-desc : random nsfw loli images
-usage : just send the command\n
-command : *${prefix}url2img*
-desc : take web screenshots
-usage : *${prefix}url2img [tipe] [url]*\n
-command : *${prefix}simi*
-desc : your message will be replied to by simi
-usage : *${prefix}simi yourmessage*\n
-command : *${prefix}ocr*
-desc : take the text in the picture
-usage : reply image, or send image with caption\n
-command : *${prefix}wait*
-desc : search anime with image [ What Anime Is This/That ]
-usage : reply image, or send image with caption\n
-command : *${prefix}setprefix*
-desc : replace prefix
-usage : *${prefix}setprefix [text|optional]*\nexample : *${prefix}setprefix ?*
-note : This command can only be used by the bot owner\n
-> *Group Comands* <
-command : *${prefix}add*
-desc : add member into group
-usage : *${prefix}add 62813xxxxx*\n
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}kick*
-desc : kick members from group
-usage : *${prefix}kick @tagmember*\n
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}promote*
-desc : make the group member as group admin
-usage : *${prefix}promote @tagmember*\n
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}demote*
-desc : make the group admin as group member
-usage : *${prefix}demote @tagmember*\n
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}linkgroup*
-desc : take the group link
-usage : just send the command
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}leave*
-desc : Make bot leave the group
-usage : just send the command
-note : Can only be used by Group admins and Bot owner\n
-command : *${prefix}tagall*
-desc : tags all group members including admins too
-usage : just send the command
-note : This command can be used if you are a group admin\n
-command : *${prefix}simih*
-desc : activate simi mode in the group
-usage : *${prefix}simih 1* to activate simi mode and *${prefix}simih 0* to deactivate simi mode
-note : This command can be used if you are a group admin\n`
+	return `
+╔═══════════════════
+║  【⛤ꦿ𝙏𝙝𝙖𝙣𝙖𝙩𝙤𝙨 𝙈𝙚𝙣𝙪⛤ꦿ𖣘
+╠═══════════════════
+║╭──❉ * ⛤𝘾𝙤𝙢𝙖𝙣𝙙𝙤𝙨⛤ * ❉──
+
+║│↭ _*prefix*  [ *${prefix}* ] 
+
+║│↭_*   [ *${prefix}hour* ] 
+║│↭ _*  [ *${prefix}dono* ] 
+║│↭_*   [ *${prefix}menuadmin*]
+║│↭_*   [ *${prefix}thanatosmenu*]
+║│↭ _*  [ *${prefix}kmkzlist* ] 
+║│↭ _*  [ *${prefix}ocr* ] 
+║│↭ _*  [ *${prefix}sticker* ] 
+║│↭_*   [ *${prefix}meme* ] 
+║│↭_*   [ *${prefix}sugestões* ]  
+║│↭_*   [ *${prefix}listadms* ] 
+║│↭_*   [ *${prefix}tagall3* ] 
+║│↭_*   [ *${prefix}toimg* ] 
+║│↭_*   [ *${prefix}tomp3* ] 
+║│↭_*   [ *${prefix}nick* ] 
+║│↭_*   [ *bot me destrava* ] 
+║│ 
+║│ Ⱑ 𝙨𝙖̃𝙤 𝙘𝙚𝙧𝙘𝙖 𝙙𝙚 150 𝙭𝙞𝙣𝙜𝙖𝙢𝙚𝙣𝙩𝙤𝙨 𝙚 𝙧𝙚𝙨𝙥𝙤𝙨𝙩𝙖𝙨 𝙖𝙪𝙩𝙤𝙢𝙖𝙩𝙞𝙘𝙖𝙨 𝙥𝙧𝙚𝙨𝙚𝙣𝙩𝙚𝙨 𝙣𝙚𝙨𝙨𝙚 𝙗𝙤𝙩 Ⱑ
+
+║ | ↭_*  *[Meu criador]*
+║https://wa.me/+553192271279
+╰───────────`
 }
 
 exports.help = help
