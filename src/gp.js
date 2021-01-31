9330187d681f26ff0b122a272da3fd7d14420625
Thanatos22 <76633120+Thanatos22@users.noreply.github.com>
2021-01-31 15:10:30 -0300

    Add files via upload

---
+++ src/gp.js
@@ -0,0 +1,14 @@
+const gp = (prefix) => {
+	return ` 
+𝑮𝑹𝑼𝑷𝑶𝑺 𝑶𝑭𝑰𝑪𝑰𝑨𝑰𝑺 𝑫𝑶 𝑩𝑶𝑻 𝑻𝑯𝑨𝑵𝑨𝑻𝑶𝑺:
+
+⛦⃕ ⃟𝑻𝑹ቾ𝑵𝑫𝛬𝐷𝛯-𝑹𝑪𝑻☦︎ ⃟⃘⃤꙰⛧  (𝙀 𝙋𝙊𝙎𝙎𝙄𝙑𝙀𝙇 𝙌𝙐𝙀 𝘼 𝙍𝘾𝙏 𝙉𝘼𝙊 𝙀𝙎𝙏𝙀𝙅𝘼 𝘼𝘽𝙀𝙍𝙏𝘼)
+ 𝙡𝙞𝙣𝙠 𝙙𝙤 𝙜𝙥: https://chat.whatsapp.com/FVdQsJQs2Xj0XKmrrZG3Gg
+ 
+ 
+ ⏤͟͟͞͞𝑪𝒉𝒂𝒕 𝑫𝒐𝒔 𝑴𝒂𝒄𝒂𝒄𝒐 
+ 𝙡𝙞𝙣𝙠 𝙙𝙤 𝙜𝙥: https://chat.whatsapp.com/CFsoBKiLqHtI4FJfc5yTHE `
+
+}
+
+exports.gp = gp
